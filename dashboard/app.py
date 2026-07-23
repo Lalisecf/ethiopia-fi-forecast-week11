a2253efa57cb5fa6706790cefd0f9f4a9afb44c1
Lalise <lalisefufi5@gmail.com>
2026-07-23 09:25:59 +0300

    task5: create main streamlit application

---
+++ dashboard/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+
+page_title="Financial Inclusion Dashboard",
+
+layout="wide"
+
+)
+st.title(
+"Forecasting Financial Inclusion in Ethiopia"
+)
+st.caption(
+"10 Academy AI Mastery Week 11"
+)
+st.sidebar.title("Navigation")
